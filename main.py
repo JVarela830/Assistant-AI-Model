44d8241737a50dba7f9f2df5483c35390ea72a86
Jose Varela <josevarela17@protonmail.com>
2026-07-14 23:40:37 +0100

    First Version of Local AI Python

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ from langchain_core.prompts import ChatPromptTemplate
 
 #Specify the model
 model = OllamaLLM(model="qwen2.5:1.5b")
+
+template = """
+You are an assistant model ready to answer questions
+
+Here is the question to answer: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+while True:
+    question = input("Ask anything (q to leave):")
+    if question == "q":
+        break
+    else:
+        result = chain.invoke({"question": question})
